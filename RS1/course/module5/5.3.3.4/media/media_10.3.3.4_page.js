694b116ae893f5f82f8fdc0bebe96459fca7f6c2
lsc72 <lsc72sio@gmail.com>
2024-09-20 09:55:35 +0200

    Delete RS1/course/module5 directory

--- RS1/course/module5/5.3.3.4/media/media_10.3.3.4_page.js
+++
@@ -1,6 +0,0 @@
-//loadScript("../../../common/scripts/templates/slide/ImageSlideNew.js");
-loadScript("../../../common/scripts/swfobject.js", registerSWF);
-
-function registerSWF(){
-    swfobject.registerObject("flashobject", "9.0.0", "../../../common/libs/expressInstall.swf");
-}
