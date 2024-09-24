100136cb2a2d3153409c5b081ac2be5146688da7
lsc72 <lsc72sio@gmail.com>
2024-09-24 08:11:50 +0200

    Update media_6.3.2.1_page.js

--- RS2/course/module6/6.3.2.1/media/media_6.3.2.1_page.js
+++ RS2/course/module6/6.3.2.1/media/media_6.3.2.1_page.js
@@ -9,7 +9,7 @@ var mainMovieClip;
 (Animation = function(control,id){
 
 var manifest = [
-		{src:"images/_5_1_2_1.jpg", id:"_5_1_2_1"},
+		{src:"images/5_1_2_1.jpg", id:"_5_1_2_1"},
 		{src:"images/Arrow.png", id:"Arrow"},
 		{src:"images/arrow2.png", id:"arrow2"},
 		{src:"images/envelope_closed_static.png", id:"envelope_closed_static"},
